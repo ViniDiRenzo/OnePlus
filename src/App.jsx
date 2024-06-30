2c8ad49e0493db83d2c5dfa97fa581f1e4513bd4
Vinicius Di Renzo <vinidirenzo@gmail.com>
2024-06-30 19:42:31 +0100

    Basic routing setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+const routes = (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+);
 
 function App() {
   return (
     <>
-      <div className="block">
-        <h1 className="text-3xl font-bold">Hello</h1>
-      </div>
+      <div className="block">{routes}</div>
     </>
   );
 }
