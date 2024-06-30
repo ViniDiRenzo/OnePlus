995fe94c81f0d353b433ae1c025da32994366f70
Vinicius Di Renzo <vinidirenzo@gmail.com>
2024-06-30 19:09:03 +0100

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="block">
+        <h1 className="text-3xl font-bold">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
