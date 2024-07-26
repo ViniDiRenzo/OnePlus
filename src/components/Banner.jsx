f32fce1ebdb49d0b5967296923f54ecb6cfe4320
Vinicius Di Renzo <vinidirenzo@gmail.com>
2024-07-26 23:28:01 +0100

    Changed Banner component.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Banner({ pageName, image }) {
+function Banner({ pageName, imageUrl, objectPosition }) {
   return (
-    <div
-      className={`block py-12 bg-[url('${image}')] bg-center text-white text-center md:text-left`}
-    >
+    <div className={`block py-12 text-white text-center md:text-left relative`}>
+      <img
+        className={`absolute inset-0 w-full h-full object-cover ${objectPosition}`}
+        src={imageUrl}
+        alt="background image"
+      />
       <h1 className="header m-0 drop-shadow-xl">{pageName}</h1>
     </div>
   );
